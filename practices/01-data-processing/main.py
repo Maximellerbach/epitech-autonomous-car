1ecd2f8e332c6ff406bf86ab49a9dcbee4d5705f
Maxime Ellerbach <maxime@ellerbach.net>
2023-03-20 17:38:13 +0100

    update

--- practices/01-data-processing/main.py
+++ practices/01-data-processing/main.py
@@ -1,5 +1,6 @@
 """
 01-data-processing
+To complete
 """
 
 import glob
@@ -14,12 +15,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 def generate_random_images(n=16, size=(256, 256, 3), save_path="data"):
-    """Generate random images"""
+    """Generate random images for the sake of the exercise"""
 
     # if data in the folder, don't generate new images
     if os.path.exists(save_path) and len(os.listdir(save_path)) > 0:
         return
-
+        
     elif not os.path.exists(save_path):
         os.mkdir(save_path)
 
@@ -31,17 +32,17 @@ def generate_random_images(n=16, size=(256, 256, 3), save_path="data"):
 
 
 def get_paths(path):
-    """Get all paths in a directory"""
-    return glob.glob(os.path.join(path, '*.png'))
+    """Get all paths finishing by .png (all the images) in the directory"""
+    return None
 
 
 def get_labels(paths):
     """
     Get labels from a path
     format: /path/to/image/imagename_label.png
+    label is an integer
     """
-    return [int(path.split(os.sep)[-1].split('_')[-1].split('.')[0]) for path in paths]
-
+    return None
 
 def onehot_labels(labels):
     """
@@ -49,58 +50,22 @@ def onehot_labels(labels):
     """
     num_classes = 10
 
-    onehot_labels = np.zeros((len(labels), num_classes))
-
-    for i, label in enumerate(labels):
-        onehot_labels[i, labels[i]] = 1
-
-    return onehot_labels
-
+    return None
 
 def load_images(paths):
     """
     load, resize, normalize all the images at once
     return a numpy array
     """
-    images = []
-    for path in paths:
-        image = cv2.imread(path)
-        image = cv2.resize(image, (128, 128))
-        image = image.astype('float32') / 255.0
-        images.append(image)
-
-    return np.array(images)
-
+    return None
 
 def vis_images(images, labels):
     """
     Visualize images with labels
+    you can use matplotlib or opencv
     """
-    import matplotlib
-    matplotlib.use('TkAgg')
-    import matplotlib.pyplot as plt
-
-    num_images = images.shape[0]
-    num_rows = int(np.sqrt(num_images))
-    num_cols = num_rows
-
-    if num_rows ** 2 < num_images:
-        num_rows += 1
-
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(10, 10))
-    for i in range(num_rows):
-        for j in range(num_cols):
-            index = i * num_cols + j
-
-            if index < num_images:
-                axes[i, j].imshow(images[index])
-                axes[i, j].set_title(labels[index])
-                axes[i, j].axis('off')
-            else:
-                axes[i, j].axis('off')
-
-    plt.show()
-
+    return None
+    
 
 if __name__ == '__main__':
     current_path = os.path.dirname(os.path.abspath(__file__))
@@ -111,11 +76,10 @@ if __name__ == '__main__':
 
     paths = get_paths(data_path)
     labels = get_labels(paths)
-    labels = onehot_labels(labels)
     images = load_images(paths)
 
     logging.info(f"paths: {paths}")
     logging.info(f"labels: {labels}")
-    logging.info(f"images: {images.shape}")
+    logging.info(f"images: {images}")
 
     vis_images(images, labels)
--- practices/01-data-processing/test.py
+++ practices/01-data-processing/test.py
@@ -6,6 +6,7 @@ except ImportError:
 import unittest
 import os
 import glob
+import numpy as np
 
 path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(path, 'data')
@@ -19,19 +20,27 @@ class Test(unittest.TestCase):
                 os.remove(path)
 
     def test_generate_random_images(self):
-        main.generate_random_images(10, save_path=data_path)
-        self.assertEqual(len(os.listdir(data_path)), 10)
+        main.generate_random_images(16, save_path=data_path)
+        self.assertEqual(len(os.listdir(data_path)), 16)
 
     def test_get_paths(self):
         paths = main.get_paths(data_path)
-        self.assertEqual(len(paths), 10)
+        self.assertEqual(len(paths), 16)
 
     def test_get_labels(self):
         paths = main.get_paths(data_path)
         labels = main.get_labels(paths)
-        self.assertEqual(len(labels), 10)
+        self.assertEqual(len(labels), 16)
+
+        onehot_labels = main.onehot_labels(labels)
+        self.assertEqual(onehot_labels.shape, (16, 10))
 
     def test_load_images(self):
         paths = main.get_paths(data_path)
         images = main.load_images(paths)
-        self.assertEqual(images.shape, (10, 128, 128, 3))
+        self.assertEqual(images.shape, (16, 128, 128, 3))
+        self.assertLessEqual(np.max(images), 1.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
