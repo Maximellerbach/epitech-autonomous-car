2a284cbb1213e420ce650afa10c061340c5e6fa6
Maxime Ellerbach <maxime@ellerbach.net>
2023-04-08 19:06:33 +0200

    test

--- practices/run_tests.sh
+++ practices/run_tests.sh
@@ -4,26 +4,25 @@
 cd practices
 
 for dir in ./*; do
-    # go into the dir
-    cd $dir
+    if [ -d "$dir" ]; then
+        echo "Testing $dir"
+        cd $dir
     
-    # run the install script
-    chmod +x install.sh
-    bash install.sh
+        # run the install script
+        chmod +x install.sh
+        ./install.sh
 
-    # activate the virtual environment
-    source venv/bin/activate
+        # activate the virtual environment
+        source venv/bin/activate
 
-    # test the script
-    python test.py
+        # test the script
+        python test.py
 
-    # deactivate the virtual environment
-    deactivate
+        # deactivate the virtual environment
+        deactivate
 
-    # cleanup
-    rm -rf venv
-
-    # go back to the root dir
-    cd ..
-
+        # cleanup
+        rm -rf venv
+        cd ..
+    fi
 done
