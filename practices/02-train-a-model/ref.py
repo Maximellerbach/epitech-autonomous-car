1fe6b2b80caabbf0685595f633348063bbfee1d7
Maxime Ellerbach <maxime@ellerbach.net>
2023-04-29 14:31:26 +0200

    added operation comparaison + bonus

--- practices/02-train-a-model/ref.py
+++ practices/02-train-a-model/ref.py
@@ -83,6 +83,100 @@ def build_model() -> tf.keras.Model:
         metrics=['accuracy']
     )
 
+    """
+    _________________________________________________________________
+    Layer (type)                Output Shape              Param #   
+    =================================================================
+    input_1 (InputLayer)        [(None, 28, 28, 1)]       0
+
+    conv2d (Conv2D)             (None, 26, 26, 32)        320       
+
+    conv2d_1 (Conv2D)           (None, 24, 24, 64)        18496     
+
+    max_pooling2d (MaxPooling2D  (None, 12, 12, 64)       0
+    )
+
+    dropout (Dropout)           (None, 12, 12, 64)        0
+
+    flatten (Flatten)           (None, 9216)              0
+
+    dense (Dense)               (None, 128)               1179776
+
+    dropout_1 (Dropout)         (None, 128)               0
+
+    dense_1 (Dense)             (None, 10)                1290
+
+    =================================================================
+    Total params: 1,199,882
+    Trainable params: 1,199,882
+    Non-trainable params: 0
+    _________________________________________________________________
+    """
+
+    ### Number of parameters
+
+    # number of parameters for convolution = (kernel_size * kernel_size * input_channels + 1) * output_channels
+    # 3 * 3 * 1 + 1 * 32 = 320
+    # 3 * 3 * 32 + 1 * 64 = 18496
+    # number of parameters for dense = (input_size + 1) * output_size
+    # (9216 + 1) * 128 = 1179776
+    # (128 + 1) * 10 = 1290
+
+    ### Number of operations
+
+    # number of operations for convolution = (kernel_size * kernel_size * input_channels) * output_channels * output_width * output_height
+    # 3 * 3 * 1 * 32 * 26 * 26 = 194688
+    # 3 * 3 * 32 * 64 * 24 * 24 = 10616832
+    # number of operations for maxpooling = (kernel_size * kernel_size * input_channels) * output_width * output_height
+    # 2 * 2 * 64 * 12 * 12 = 36864
+    # number of operations for dense = (input_size + 1) * output_size
+    # (9216 + 1) * 128 = 1179776
+    # (128 + 1) * 10 = 1290
+    # total: 12029450 operations
+
+    return model
+
+
+def build_model_strides() -> tf.keras.Model:
+    """
+    alternative to build_model using strides instead of maxpooling
+    """
+
+    inp = tf.keras.layers.Input(shape=(28, 28, 1))
+    x = tf.keras.layers.Conv2D(32, 3, activation='relu')(inp)
+    x = tf.keras.layers.Conv2D(64, 3, strides=(2, 2), activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.25)(x)
+
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    out = tf.keras.layers.Dense(10, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs=inp, outputs=out)
+
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    ### Number of parameters same as build_model
+
+    ### Number of operations
+
+    # number of operations for convolution = (kernel_size * kernel_size * input_channels) * output_channels * output_width * output_height
+    # 3 * 3 * 1 * 32 * 26 * 26 = 194688
+    # 3 * 3 * 32 * 64 * 12 * 12 = 2654208
+    # number of operations for dense = (input_size + 1) * output_size
+    # (9216 + 1) * 128 = 1179776
+    # (128 + 1) * 10 = 1290
+    # total: 4031252 operations
+
+    ###############################
+    # simply by replacing the maxpooling layer with a stride of 2,
+    # we reduced the number of operations by a factor of 3 WITHOUT reducing the number of parameters
+    ###############################
+
     return model
 
 
@@ -136,6 +230,7 @@ def train_model(
         batch_size=batchsize,
         **kwargs
     )
+    return model
 
 
 def predict(model: tf.keras.Model, x: np.ndarray, sample_size: int):
---
+++ practices/02-train-a-model/ref_bonus.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def predict_from_path(image_path, model) -> int:
+    """
+    Load, resize, reshape, normalize the image and predict the class.
+    """
+
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, (28, 28))
+    x = image.reshape(1, 28, 28, 1) # add batch dimension and grayscale channel
+
+    # normalize the data
+    x = x / 255.0
+
+    # predict
+    pred = model.predict(x)
+    prediction = np.argmax(pred)
+
+    plt.imshow(image)
+    plt.title(f"Prediction: {prediction}")
+    plt.show()
+
+if __name__ == "__main__":
+    # load the model
+    model = tf.keras.models.load_model("model.h5")
+
+    # predict
+    predict_from_path("test.png", model)
+
